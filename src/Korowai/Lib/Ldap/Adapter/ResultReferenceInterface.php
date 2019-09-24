3be1fa40ebdc27db9d2afbfc23dce6adcf31bf2a
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2019-09-24 15:54:38 +0200

    moved all interfaces to korowai\contracts package

---
+++ src/Korowai/Lib/Ldap/Adapter/ResultReferenceInterface.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @file src/Korowai/Lib/Ldap/Adapter/ResultReferenceInterface.php
+ *
+ * This file is part of the Korowai package
+ *
+ * @author Paweł Tomulik <ptomulik@meil.pw.edu.pl>
+ * @package korowai\contracts
+ * @license Distributed under MIT license.
+ */
+
+declare(strict_types=1);
+
+namespace Korowai\Lib\Ldap\Adapter;
+
+/**
+ * @author Paweł Tomulik <ptomulik@meil.pw.edu.pl>
+ */
+interface ResultReferenceInterface
+{
+    /**
+     * Returns referrals
+     * @return array An array of referrals
+     */
+    public function getReferrals();
+}
+
+// vim: syntax=php sw=4 ts=4 et:
